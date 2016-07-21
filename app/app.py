0e65af9bf31fbd96e7e392af54abd710dc6132cf
Jani Kenttälä <janike@iki.fi>
2016-07-21 12:44:08 +0300

    Tox runs tests now. Python3 compatibility added.

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 def math_science(value):
     """
     >>> math_science(1)
-    3
+    2
     """
     return 1 + value
 
 if __name__ == "__main__":
-    print math_science(1)
+    print(math_science(1))
